710593bcf562a22395801bba3b7ecbdcfbbff64f
Tony Hart <154579112+thaarrtt@users.noreply.github.com>
2024-07-17 08:38:23 +0700

    update ip listener

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ mod services;
 #[tokio::main]
 async fn main() {
 
-    let listener = TcpListener::bind("127.0.0.1:8080")
+    let listener = TcpListener::bind("0.0.0.0:8080")
         .await
         .expect("Unable to conne to connect to the server");
 
